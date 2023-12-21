17210cf7e269e0349e46f18eb607d54228a52107
Alan Martins de Oliveira <alan.oliveira@fitbank.com.br>
2023-12-21 15:48:24 -0300

    feat: Foi Realizada a criação da API.

---
+++ RestWithASPNET/RestWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {          
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                return BadRequest("Invalid Input");
+            }
+        }
+
+        public bool IsNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
